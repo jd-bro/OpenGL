af8f2c332a5d06068b22a6b0deed89f00cc4dc54
jd-bro <mail2me.jayadev@gmail.com>
2020-11-14 19:21:08 +0530

    3 qn

---
+++ Q3/q3.c
@@ -0,0 +1,58 @@
+#include<GL/freeglut.h>
+#include<GL/gl.h>
+#include<stdio.h>
+
+struct Point
+{
+	GLfloat x,y;
+};
+
+struct  Point p[100000];
+int count = 0;
+
+void readInput()
+{
+	FILE *fptr = fopen("input.txt", "r");
+	if(fptr)
+	{
+
+		while(fscanf(fptr, "%f %f", &(p[count].x), &(p[count].y))!=EOF)
+		{
+			count++;
+		}
+		fclose(fptr);
+	}
+}
+void renderFunction()
+{
+	int i=0;
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.0, 0.0, 0.0);
+	glPointSize(5);
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+
+	glBegin(GL_LINES);
+	while(i<count-1)
+	{
+		glVertex2f(p[i].x,p[i].y);
+		glVertex2f(p[i+1].x,p[i+1].y);
+		i++;
+
+	}
+	glEnd();
+	glFlush();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE);
+	glutInitWindowSize(500,500);
+	glutInitWindowPosition(100,100);
+	readInput();
+	glutCreateWindow("OpenGL - First Window Demo");
+	glutDisplayFunc(renderFunction);
+	glutMainLoop();
+	return 0;
+}
